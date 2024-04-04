893431df9b052ba40de0e4b9961f4c9f24e0aeea
AlbertoF17 <albertofergom@gmail.com>
2024-04-04 13:25:58 +0200

    Ejercicio Maven

---
+++ src/main/java/es/babel/Main.java
@@ -0,0 +1,24 @@
+package es.babel;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+public class Main {
+    public static void main(String[] args) throws JsonSyntaxException {
+        Gson gson = new Gson();
+
+        Vehiculo vehiculo = new Vehiculo("Renault", "Cactus");
+
+        String jsonVehiculo = gson.toJson(vehiculo);
+        System.out.println("Objeto Vehículo convertido a JSON:");
+        System.out.println(jsonVehiculo);
+
+        System.out.println("#####################################");
+
+        Vehiculo vehiculoFromJson = gson.fromJson(jsonVehiculo, Vehiculo.class);
+        System.out.println("JSON convertido de vuelta a objeto Vehículo:");
+        System.out.println("Marca: " + vehiculoFromJson.getMarca());
+        System.out.println("Modelo: " + vehiculoFromJson.getModelo());
+    }
+}
---
+++ src/main/java/es/babel/Vehiculo.java
@@ -0,0 +1,19 @@
+package es.babel;
+
+public class Vehiculo {
+    private String marca;
+    private String modelo;
+
+    public Vehiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+}
